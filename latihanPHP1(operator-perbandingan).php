d238d39bdf64a276e81c54c1736ebda50c1d6024
Zulka <zulka.ajuab@gmail.com>
2021-03-17 14:16:36 +0700

    Initial Commit

---
+++ latihanPHP1(operator-perbandingan).php
@@ -0,0 +1,20 @@
+<?php
+$a = "5";
+$b = 5;
+
+echo "Variabel a :$a <br/>";
+echo "Variabel b :$b <br/>";
+if($a != $b){
+	echo "Variabel a equal dengan variabel b";
+}else{
+	echo "Variabel a tidak equal dengan variabel b";
+}
+echo "<br/>";
+if($a === $b){
+	echo "Variabel a identical dengan variabel b";
+}else{
+	echo "Variabel a tidak identical dengan variabel b";
+}
+
+
+?>
---
+++ latihanPHP2(operator-logika).php
@@ -0,0 +1,15 @@
+<?php
+//input dari user
+$username = "admin";
+$password = "passwordAdmin";
+
+//proses dan output
+if($username == "admin" && $password == "passwordAdmin"){
+    echo "Anda berhak masuk sistem";
+}else{
+    echo "Anda tidak berhak masuk sistem";
+}
+
+
+
+?>
